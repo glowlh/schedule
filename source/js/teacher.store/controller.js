d7db3e09f72b38fc00a74b76cd5c641f64455a86
glowlh <m.o.gushchina@gmail.com>
2017-06-27 20:08:14 +0300

    TeacherStore: remove methods for finding and deleting items. Update add method

--- source/js/teacher.store/controller.js
+++ source/js/teacher.store/controller.js
@@ -1,5 +1,4 @@
 import Store from '../store/controller';
-import Teacher from '../teacher/controller';
 
 const PREFIX_ID = 'teacher';
 
@@ -10,38 +9,7 @@ class TeacherStore extends Store {
   }
 
   add(data) {
-    const teacher = new Teacher(data);
-    const name = teacher.name;
-    if (this.find(name)) {
-      return;
-    }
-
-    const id = this._generateId(PREFIX_ID);
-    const item = {
-      id,
-      teacher,
-    };
-    this.items.set(id, item);
-  }
-
-  delete(name) {
-    const item = this.find(name);
-    if (!item) {
-      return;
-    }
-
-    this.items.delete(item.id);
-  }
-
-  find(name) {
-    let result = null;
-    this.items.forEach((it) => {
-      if (it.teacher.name === name) {
-        result = it;
-      }
-    });
-
-    return result;
+    super.add(data, PREFIX_ID);
   }
 }
 
